c8e95c9abb5e62c8c23b0c3ed1531c18d13cc675
Ray-M05 <rachelmojena@gmail.com>
2024-12-23 17:39:10 -0500

    Initial Template

---
+++ engine/__init__.py
@@ -0,0 +1,4 @@
+from .database import BBDD, InfoDocument
+from .search import SearchEngine, SearchItem, SearchResult
+
+__all__ = ["BBDD", "InfoDocument", "SearchEngine", "SearchItem", "SearchResult"]
